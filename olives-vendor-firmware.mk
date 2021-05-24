dde297800ff87d469cf6c82450fe0578045e4f9e
zqm64 <zqm64@outlook.com>
2021-05-24 23:57:27 +0800

    olives: Import firmwares

---
+++ olives-vendor-firmware.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/olives/firmware/vendor/firmware/aw87519_rcv.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw87519_rcv.bin \
+    vendor/xiaomi/olives/firmware/vendor/firmware/aw87519_spk.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw87519_spk.bin \
+    vendor/xiaomi/olives/firmware/vendor/firmware/focaltech_ts_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/focaltech_ts_fw.bin \
+    vendor/xiaomi/olives/firmware/vendor/firmware/novatek_ts_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/novatek_ts_fw.bin \
+    vendor/xiaomi/olives/firmware/vendor/firmware/novatek_ts_mp.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/novatek_ts_mp.bin
