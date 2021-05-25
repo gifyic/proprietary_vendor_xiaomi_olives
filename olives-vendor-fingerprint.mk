cf9a6b4084657994993d78e811b3cffcfbb85dfc
zqm64 <zqm64@outlook.com>
2021-05-25 15:07:59 +0800

    olives: Import fingerprint

---
+++ olives-vendor-fingerprint.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/com.fingerprints.extension@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/com.fingerprints.extension@1.0.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/hw/fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.default.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/hw/gf_fingerprint.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gf_fingerprint.default.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/libgf_ca.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_ca.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_hal.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/libgoodixhwfingerprint.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgoodixhwfingerprint.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib/com.fingerprints.extension@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/com.fingerprints.extension@1.0.so
--- olives-vendor.mk
+++ olives-vendor.mk
@@ -7,5 +7,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Inherit fragments
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor-acdb.mk)
+$(call inherit-product, vendor/xiaomi/olives/olives-vendor-fingerprint.mk)
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor-firmware.mk)
 $(call inherit-product, vendor/xiaomi/olives/olives-vendor-ir.mk)
