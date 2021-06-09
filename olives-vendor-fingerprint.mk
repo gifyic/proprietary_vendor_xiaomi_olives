56fcefdffe592e42b4a817474d7806c1e5af5c8b
zqm64 <zqm64@outlook.com>
2021-06-09 18:08:03 +0800

    [FIXUP] olives: fp: add missing goodix blob

--- olives-vendor-fingerprint.mk
+++ olives-vendor-fingerprint.mk
@@ -6,4 +6,5 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/olives/fingerprint/vendor/lib64/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_hal.so \
     vendor/xiaomi/olives/fingerprint/vendor/lib64/libgoodixhwfingerprint.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgoodixhwfingerprint.so \
     vendor/xiaomi/olives/fingerprint/vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so \
+    vendor/xiaomi/olives/fingerprint/vendor/lib64/vendor.goodix.hardware.biometrics.fingerprint@2.1.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.goodix.hardware.biometrics.fingerprint@2.1.so \
     vendor/xiaomi/olives/fingerprint/vendor/lib/com.fingerprints.extension@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/com.fingerprints.extension@1.0.so
